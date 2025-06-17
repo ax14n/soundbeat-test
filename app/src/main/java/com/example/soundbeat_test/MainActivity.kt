a89f3306ac4a21315a5a2eb778b7c5f5b7171565
Zelmar Hernan <zelmarhernanramilopiazzoli2202@gmail.com>
2025-06-17 13:35:43 +0200

    Agrega una guía básica al principio de la APP

--- app/src/main/java/com/example/soundbeat_test/MainActivity.kt
+++ app/src/main/java/com/example/soundbeat_test/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.soundbeat_test
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,11 +18,17 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -61,6 +71,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SoundBeat_TestTheme {
+
                 val audioPlayerViewModel: AudioPlayerViewModel = viewModel()
                 val sharedPlaylistViewModel: SharedPlaylistViewModel = viewModel()
                 val playlistScreenViewModel: PlaylistScreenViewModel = viewModel()
@@ -225,25 +236,62 @@ fun MainScreen(
     playlistScreenViewModel: PlaylistScreenViewModel?
 ) {
     val navItemList: List<NavItem> = GetNavItemList()
+    var selectedIndex by remember { mutableIntStateOf(0) }
 
-    var selectedIndex by remember {
-        mutableIntStateOf(0)
-    }
+    var showcaseStep by remember { mutableIntStateOf(0) }
+    val modifier: Modifier = Modifier.clickable {}
 
-    Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
-        BottomNavigationBar(navItemList, selectedIndex) { selectedIndex = it }
-    }) { it ->
-        val padding = it
-        Column() {
-            ContentScreen(
-                selectedIndex = selectedIndex,
-                navHostController = navHostController,
-                playlistScreenViewModel = playlistScreenViewModel,
-                sharedPlaylistViewModel = sharedPlaylistViewModel
+    val context: Context = LocalContext.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(if (showcaseStep <= 3) modifier else Modifier)
+    ) {
+        Scaffold(bottomBar = {
+            BottomNavigationBar(navItemList, selectedIndex) { selectedIndex = it }
+        }) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                ContentScreen(
+                    selectedIndex = selectedIndex,
+                    navHostController = navHostController,
+                    playlistScreenViewModel = playlistScreenViewModel,
+                    sharedPlaylistViewModel = sharedPlaylistViewModel
+                )
+            }
+        }
+
+        val prefs = remember { context.getSharedPreferences("UserInfo", Context.MODE_PRIVATE) }
+        val tutorial = remember {
+            derivedStateOf {
+                prefs.getString("tutorial", "ON")
+            }
+        }.value
+
+        if (showcaseStep <= 3 && tutorial != "OFF") {
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    // Con esto bloqueo los inputs para que no pasen por detrás cuando la guía está
+                    // en proceso.
+                    .pointerInput(Unit) {
+                        awaitPointerEventScope {
+                            while (true) {
+                                awaitPointerEvent()
+                            }
+                        }
+                    })
+
+            // Tu guía
+            Guide(
+                step = showcaseStep,
+                onNext = {
+                    showcaseStep++
+                },
+                context = context,
             )
         }
     }
-
 }
 
 /**
@@ -302,5 +350,69 @@ fun ContentScreen(
         )
 
         3 -> ProfileScreen(navHostController)
+    }
+}
+
+@Composable
+fun Guide(
+    step: Int,
+    modifier: Modifier = Modifier,
+    onNext: () -> Unit,
+    context: Context,
+) {
+    val prefs = remember { context.getSharedPreferences("UserInfo", Context.MODE_PRIVATE).edit() }
+
+    when (step) {
+        0 -> Box(modifier = modifier.fillMaxSize()) {
+            Text(
+                "¡Aquí puedes ver las canciones disponibles!",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+                    .background(Color.Black.copy(alpha = 0.8f))
+                    .clickable { onNext() }
+                    .padding(16.dp),
+                color = Color.White)
+        }
+
+        1 -> Box(modifier = modifier.fillMaxSize()) {
+            Text(
+                "¡Aquí puedes ver las playlists locales y remotas!.",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+                    .background(Color.Black.copy(alpha = 0.8f))
+                    .clickable { onNext() }
+                    .padding(16.dp),
+                color = Color.White)
+        }
+
+        2 -> Box(modifier = modifier.fillMaxSize()) {
+            Text(
+                "¡Aquí puedes ver ver le búscador de canciones!",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+                    .background(Color.Black.copy(alpha = 0.8f))
+                    .clickable { onNext() }
+                    .padding(16.dp),
+                color = Color.White)
+        }
+
+        3 -> Box(modifier = modifier.fillMaxSize()) {
+            Text(
+                "¡Aquí he tu perfil!",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+                    .background(Color.Black.copy(alpha = 0.8f))
+                    .clickable { onNext() }
+                    .padding(16.dp),
+                color = Color.White)
+            prefs.putString("tutorial", "OFF").apply()
+
+        }
+
+        else -> {}
     }
 }
